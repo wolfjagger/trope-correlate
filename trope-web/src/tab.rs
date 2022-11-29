3b50ea12c1cd070556a609c73fd94e9a80d8a318
joe <joe@mortalarc.com>
2022-11-29 15:33:44 -0600

    Implement mouseover css

--- trope-web/src/tab.rs
+++ trope-web/src/tab.rs
@@ -13,9 +13,38 @@ pub struct TabProps {
 #[function_component]
 pub fn Tab(props: &TabProps) -> Html {
 
+  let mouseover = use_state_eq(|| false);
+
+  let onmouseover = {
+    let mouseover = mouseover.clone();
+    move |_| {
+      mouseover.set(true);
+    }
+  };
+
+  let onmouseout = {
+    let mouseover = mouseover.clone();
+    move |_| {
+      mouseover.set(false);
+    }
+  };
+
+  let req_classes = vec!["label-button", "horiz"];
+  let inactive_classes = [req_classes.clone(), vec!["inactive-button-style"]].concat();
+  let mouseover_classes = [req_classes, vec!["mouseover-button-style"]].concat();
+
   html! {
     <Link<route::Route> classes={classes!("tab-link")} to={props.route.clone()}>
-      <label class="label-button inactive-button-style horiz">{ &props.title }</label>
+      <label
+        class={
+          if *mouseover {
+            classes!(mouseover_classes)
+          } else {
+            classes!(inactive_classes)
+          }
+        }
+        { onmouseover } { onmouseout }
+      >{ &props.title }</label>
     </Link<route::Route>>
   }
 
