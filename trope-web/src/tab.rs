e79c3e67d1116094fb8a7a506988308a9a3c7ec2
joe <joe@mortalarc.com>
2022-11-29 16:57:18 -0600

    Add button is_active property, implemented when you have mousedowned

--- trope-web/src/tab.rs
+++ trope-web/src/tab.rs
@@ -13,37 +13,60 @@ pub struct TabProps {
 #[function_component]
 pub fn Tab(props: &TabProps) -> Html {
 
-  let mouseover = use_state_eq(|| false);
+  // Mouse events
+
+  let is_mouseover = use_state_eq(|| false);
+  let is_active = use_state_eq(|| false);
 
   let onmouseover = {
-    let mouseover = mouseover.clone();
+    let is_mouseover = is_mouseover.clone();
     move |_| {
-      mouseover.set(true);
+      is_mouseover.set(true);
     }
   };
 
   let onmouseout = {
-    let mouseover = mouseover.clone();
+    let is_mouseover = is_mouseover.clone();
     move |_| {
-      mouseover.set(false);
+      is_mouseover.set(false);
     }
   };
 
+  let onmousedown = {
+    let is_active = is_active.clone();
+    move |_| {
+      is_active.set(true);
+    }
+  };
+
+  let onmouseup = {
+    let is_active = is_active.clone();
+    move |_| {
+      is_active.set(false);
+    }
+  };
+
+  // CSS
   let req_classes = vec!["label-button", "horiz"];
+  let active_classes = [req_classes.clone(), vec!["active-button-style"]].concat();
   let inactive_classes = [req_classes.clone(), vec!["inactive-button-style"]].concat();
   let mouseover_classes = [req_classes, vec!["mouseover-button-style"]].concat();
 
+  // HTML
   html! {
     <Link<route::Route> classes={classes!("tab-link")} to={props.route.clone()}>
       <label
         class={
-          if *mouseover {
+          if *is_active {
+            classes!(active_classes)
+          } else if *is_mouseover {
             classes!(mouseover_classes)
           } else {
             classes!(inactive_classes)
           }
         }
         { onmouseover } { onmouseout }
+        { onmousedown } { onmouseup }
       >{ &props.title }</label>
     </Link<route::Route>>
   }
