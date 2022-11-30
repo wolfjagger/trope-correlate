f18d02a12885e9dc809a111bf9a62c03e552c7f8
joe <joe@mortalarc.com>
2022-11-30 02:27:31 -0600

    Commentary

--- trope-web/src/route/home.rs
+++ trope-web/src/route/home.rs
@@ -6,8 +6,12 @@ use yew::prelude::*;
 #[function_component]
 pub fn Home() -> Html {
 
-  let svg_str = use_state_eq(|| draw(1));
+  // svg_str the calculated svg string to draw
+  let svg_str = use_state_eq(|| create_plot_svg_str(1));
+  // svg_ref a NodeRef to the svg element in html
   let svg_ref = use_node_ref();
+
+  // Replace svg_ref content with svg_str
   {
     let svg_str = svg_str.clone();
     let svg_ref = svg_ref.clone();
@@ -36,11 +40,14 @@ pub fn Home() -> Html {
 }
 
 
-/// Draw power function f(x) = x^power.
-pub fn draw(power: i32) -> String {
+/// Create a svg string for a plot of the power function f(x) = x^power.
+pub fn create_plot_svg_str(power: i32) -> String {
 
+  // TODO: Figure out where to move the size handling
   let size = (480, 480);
   let mut svg_str = String::new();
+
+  // Get svg backend and associate it with svg_str
   let backend = SVGBackend::with_string(&mut svg_str, size);
 
   draw_on_backend(backend, power).expect("draw_on_backend error");
@@ -49,14 +56,16 @@ pub fn draw(power: i32) -> String {
 
 }
 
+/// Draw power function on the given backend
 fn draw_on_backend<Backend>(backend: Backend, power: i32) -> Result<(), DrawingAreaErrorKind<Backend::ErrorType>>
 where Backend: plotters::prelude::DrawingBackend {
 
+  // Get drawing area, set font and background
   let root = backend.into_drawing_area();
   let font: FontDesc = ("sans-serif", 20.0).into();
-
   root.fill(&WHITE)?;
 
+  // Build chart components without data
   let mut chart = ChartBuilder::on(&root)
     .margin(20)
     .caption(format!("y=x^{}", power), font)
@@ -64,8 +73,10 @@ where Backend: plotters::prelude::DrawingBackend {
     .y_label_area_size(30)
     .build_cartesian_2d(-1f32..1f32, -1.2f32..1.2f32)?;
 
+  // Configure the mesh
   chart.configure_mesh().x_labels(3).y_labels(3).draw()?;
 
+  // Plot the power function data
   chart.draw_series(LineSeries::new(
     (-50..=50)
       .map(|x| x as f32 / 50.0)
@@ -73,6 +84,7 @@ where Backend: plotters::prelude::DrawingBackend {
     &RED,
   ))?;
 
+  // Present changes to the backend
   root.present()?;
 
   Ok(())
