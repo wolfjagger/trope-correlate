4ad9c77f763dae431a5908f6b0d9a5678de0dc16
Trystan Koch <Trystan.Koch@gmail.com>
2022-12-12 13:20:49 -0500

    Curl commands now generated by a function. Arguments are now parsed.

--- trope-download/trope-downloader.py
+++ trope-download/trope-downloader.py
@@ -1,56 +1,71 @@
 """A quick script to help download TvTropes pages with curl."""
 
+import argparse
 import subprocess
+from functools import partial
 from os import makedirs
 from os.path import join
 from time import sleep
 
 
-CURL_COMMAND: list[str] = [
-    "curl",
-    "--connect-timeout",
-    "60",
-    "-G",
-    "-d",
-    "",
-    "-d",
-    "",
-    "-d",
-    "",
-    "https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php",
-]
-
 NAMESPACE_PREFIX = "n="
-NAMESPACE_INDEX = 5
-PAGE_TYPE_PREFIX = "t="
-PAGE_TYPE_INDEX = 7
-PAGE_PREFIX = "page="
-PAGE_INDEX = 9
+PAGETYPE_PREFIX = "t="
+PAGENUM_PREFIX = "page="
+TVTROPES_SEARCH_PAGE = (
+    "https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php"
+)
 
 
 def main() -> None:
     """Download all the pages."""
-    namespace = "Main"
-    page_type = "trope"
+    args = create_arg_parser().parse_args()
 
-    path_dir: str = join("test_data", namespace + "_" + page_type, "")
+    path_dir: str = join("test_data", args.namespace + "_" + args.pagetype, "")
     makedirs(path_dir, exist_ok=True)
-
-    curl_command = CURL_COMMAND.copy()
-    curl_command[NAMESPACE_INDEX] = NAMESPACE_PREFIX + namespace
-    curl_command[PAGE_TYPE_INDEX] = PAGE_TYPE_PREFIX + page_type
 
     page = 1
 
-    while page < 59:
-        curl_command[PAGE_INDEX] = PAGE_PREFIX + str(page)
+    page_command = partial(create_curl_command, args.namespace, args.pagetype)
+
+    while page <= args.max_pages:
+        curl_command = page_command(str(page))
         filename = path_dir + "page" + str(page) + ".html"
         print(curl_command)
         html = subprocess.run(curl_command, check=False, capture_output=True, text=True)
         with open(filename, "w") as file:
             file.write(html.stdout)
         page += 1
-        sleep(0.5)
+        sleep(1)
+
+
+def create_curl_command(namespace: str, pagetype: str, page: str) -> list[str]:
+    """Create a command for curl to download a tvtropes page."""
+    command: list[str] = [
+        "curl",
+        "--connect-timeout",
+        "60",
+        "-G",
+        "-d",
+        NAMESPACE_PREFIX + namespace,
+        "-d",
+        PAGETYPE_PREFIX + pagetype,
+        "-d",
+        PAGENUM_PREFIX + page,
+        TVTROPES_SEARCH_PAGE,
+    ]
+    return command
+
+
+def create_arg_parser() -> argparse.ArgumentParser:
+    """Create an object to parse the arguments."""
+    parser = argparse.ArgumentParser(
+        prog="trope-downloader",
+        description="Downloads index pages in bulk from tvtropes.",
+    )
+    parser.add_argument("-n", "--namespace", default="Main")
+    parser.add_argument("-t", "--pagetype", default="trope")
+    parser.add_argument("--max_pages", default=2)
+    return parser
 
 
 if __name__ == "__main__":
