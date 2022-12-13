26f623304255ec9bce1e12738da6383c769f7def
joe <joe@mortalarc.com>
2022-12-13 00:31:51 -0600

    Change Home to a struct component, start a tick & animation, and update the svg at each step

--- trope-web/src/route/home.rs
+++ trope-web/src/route/home.rs
@@ -1,44 +1,84 @@
+use gloo_timers::callback::Interval;
 use web_sys::Element;
 use yew::prelude::*;
 
-use crate::plot::SvgPlot;
+use crate::{message::Message, plot::{SvgPlot, PlotType}};
 
 
-#[function_component]
-pub fn Home() -> Html {
+const SIM_TIME_MILLI: u32 = 500;
+const SIM_SPEED: f64 = 0.035;
 
-  let svg_plot = use_state(|| SvgPlot::undirected_graph());
-  // div_svg_ref a NodeRef to the element in html that will contain the svg
-  let div_svg_ref = use_node_ref();
 
-  // Fill div_svg_ref content with calculated svg
-  use_effect_with_deps(
-    |(svg_plot, div_svg_ref)| {
+pub struct Home {
+  svg_anim: SvgPlot,
+  div_svg_ref: NodeRef,
+  _interval: Interval,
+}
 
-      let div_svg = div_svg_ref.cast::<Element>()
-        .expect("svg_ref not attached to svg element");
+impl Component for Home {
 
-      // svg_str the calculated svg string to draw
-      let svg_str = svg_plot.print_to_string();
+  type Message = Message;
+  type Properties = ();
+
+  fn create(ctx: &Context<Self>) -> Self {
+
+    let svg_anim = SvgPlot::undirected_graph();
+    // div_svg_ref a NodeRef to the element in html that will contain the svg
+    let div_svg_ref = NodeRef::default();
+
+    let callback = ctx.link().callback(|_| {
+      Message::Tick
+    });
+    let interval = Interval::new(SIM_TIME_MILLI, move || callback.emit(()));
+
+    Self {
+      svg_anim,
+      div_svg_ref,
+      _interval: interval,
+    }
+
+  }
+
+  fn rendered(&mut self, _ctx: &Context<Self>, _first_render: bool) {
+    // Fill div_svg_ref content with calculated svg
+    if let Some(div_svg) = self.div_svg_ref.cast::<Element>() {
+      let svg_str = self.svg_anim.print_to_string();
       div_svg.set_inner_html(&svg_str);
+    }
+  }
 
-    },
-    (svg_plot.clone(), div_svg_ref.clone())
-  );
+  fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    match msg {
+      Message::Tick => {
+        match &mut self.svg_anim.plot_type {
+          PlotType::PowerFn(_) => false,
+          PlotType::DirectedPetGraph(g) => {
+            g.update_simulation(SIM_SPEED);
+            true
+          },
+          PlotType::UndirectedPetGraph(g) => {
+            g.update_simulation(SIM_SPEED);
+            true
+          }
+        }
+      }
+    }
+  }
 
-  // Mouse events
+  fn view(&self, _ctx: &Context<Self>) -> Html {
 
-  // HTML
-  html! {
-    <>
+    html! {
+      <>
 
-      <h1>{ "Home" }</h1>
+        <h1>{ "Home" }</h1>
 
-      <h2>{ "Woah, incredible splash graph!" }</h2>
+        <h2>{ "Woah, incredible splash graph!" }</h2>
 
-      <div ref={div_svg_ref}/>
+        <div ref={self.div_svg_ref.clone()}/>
 
-    </>
+      </>
+    }
+
   }
 
 }
