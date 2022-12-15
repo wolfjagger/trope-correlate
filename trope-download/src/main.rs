dde78bb0ac51dbe0bdeef15249591cf92122dba4
joe <joe@mortalarc.com>
2022-12-14 21:32:42 -0600

    Change from raw strings to use reqwest & url

--- trope-download/src/main.rs
+++ trope-download/src/main.rs
@@ -8,9 +8,9 @@ use reqwest;
 use arg::Args;
 
 
-const NAMESPACE_PREFIX: &str = "n=";
-const PAGETYPE_PREFIX: &str = "t=";
-const PAGENUM_PREFIX: &str = "page=";
+const NAMESPACE_PREFIX: &str = "n";
+const PAGETYPE_PREFIX: &str = "t";
+const PAGENUM_PREFIX: &str = "page";
 const TVTROPES_SEARCH_PAGE: &str =
   "https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php";
 
@@ -30,13 +30,18 @@ fn download_all_pages() -> Result<(), Box<dyn std::error::Error>> {
   fs::create_dir_all(&path_dir)?;
 
   for page in 1..args.max_pages+1 {
+
     let page_str = page.to_string();
     let mut filename = path_dir.clone();
     filename.push(format!("page{}.html", &page_str));
-    let url = create_url(&args.namespace, &args.pagetype, &page_str);
-    let body = reqwest::blocking::get(url)?.text()?;
+
+    let url = create_url(&args.namespace, &args.pagetype, &page_str)?;
+
+    let body = get_body(url)?;
+
     fs::write(filename, body)?;
     thread::sleep(time::Duration::from_secs(1));
+
   }
 
   Ok(())
@@ -45,15 +50,14 @@ fn download_all_pages() -> Result<(), Box<dyn std::error::Error>> {
 
 
 /// Define the url string from the query arguments.
-fn create_url<'a>(namespace: &'a str, pagetype: &'a str, page: &'a str) -> String {
-  format!(
-    "{}?{}{}&{}{}&{}{}",
+fn create_url(namespace: &str, pagetype: &str, page: &str) -> Result<reqwest::Url, url::ParseError> {
+  reqwest::Url::parse_with_params(
     TVTROPES_SEARCH_PAGE,
-    NAMESPACE_PREFIX,
-    namespace,
-    PAGETYPE_PREFIX,
-    pagetype,
-    PAGENUM_PREFIX,
-    page
+    &[(NAMESPACE_PREFIX, namespace), (PAGETYPE_PREFIX, pagetype), (PAGENUM_PREFIX, page)]
   )
+}
+
+
+fn get_body(url: reqwest::Url) -> Result<String, reqwest::Error> {
+  reqwest::blocking::get(url)?.text()
 }
