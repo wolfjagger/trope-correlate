f1f72e1f123a0df541a22d856bda970bbfab193c
joe <joe@mortalarc.com>
2022-12-14 23:43:04 -0600

    Add empty header file and readme describing how to add header information

---
+++ trope-download/readme.md
@@ -0,0 +1,13 @@
+Trope Download
+===
+
+Download tropes from tvtropes.org.
+
+Header file
+---
+
+To run, you need header information in the local `.header` file.
+First, load tvtropes.org, e.g. https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?n=Main&t=trope&page=1.
+Open the browser console (ctrl+shift+i).
+Find the "request header" (in firefox, the "Network tab", first entry, toggle "Raw").
+Copy the header from "User-Agent" onward into the `.header` file.
--- trope-download/src/main.rs
+++ trope-download/src/main.rs
@@ -67,10 +67,13 @@ fn download_all_pages() -> Result<(), Box<dyn std::error::Error>> {
 fn get_header_map() -> reqwest::header::HeaderMap {
 
   let header_str = fs::read_to_string(".header").expect("You need to set the .header file; see readme");
-  let mut map = reqwest::header::HeaderMap::new();
-  let header_lines = header_str.lines();
+  if header_str.is_empty() {
+    panic!("Your .header file is empty; see readme");
+  }
 
-  for header_line in header_lines {
+  let mut map = reqwest::header::HeaderMap::new();
+
+  for header_line in header_str.lines() {
     let (key, val) = header_line.split_once(": ").expect("A header line is improperly formatted");
     let header_name = reqwest::header::HeaderName::from_bytes(key.as_bytes()).expect("Incorrect header name in header file");
     let header_value = val.parse().expect("Header value could not be parsed");
