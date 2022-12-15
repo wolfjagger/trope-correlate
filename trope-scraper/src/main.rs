7d2a33ef380f97093c3c677bd01b69c7923efb2e
joe <joe@mortalarc.com>
2022-12-15 15:58:22 -0600

    Write rust-scraper to use files from shared dir & decompress with brotli if encrypted

--- trope-scraper/src/main.rs
+++ trope-scraper/src/main.rs
@@ -1,106 +1,126 @@
-// use reqwest::Response;
-// use scraper::{Html, Selector};
-// use std::fmt;
-// use std::fs::File;
-// use std::io::prelude::*;
-// use std::path::Path;
+mod arg;
 
-// #[derive(Debug)]
-// struct Trope {
-//     name: String,
-//     url: String,
-// }
+use std::{fmt, fs, io::{prelude::*, BufWriter}, path};
+use brotli::BrotliDecompress;
+use scraper::{Html, Selector};
 
-// impl fmt::Display for Trope {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         // Write strictly the first element into the supplied output
-//         // stream: `f`. Returns `fmt::Result` which indicates whether the
-//         // operation succeeded or failed. Note that `write!` uses syntax which
-//         // is very similar to `println!`.
-//         write!(f, "[{},{}]", self.name, self.url)
-//     }
-// }
+use trope_lib;
+use crate::arg::Args;
 
-fn main() {
-    /*
-    //This holds the root page
-    let root_page: String = String::from("https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?n=Main&t=trope&page=");
 
-    // Eventually, this will be in a loop over page numbers
-    let mut page: String = root_page.clone() + &1.to_string();
-
-    // Get the page
-    let response = reqwest::blocking::get(&page).unwrap().text().unwrap();
-
-    // lol, they block robots...
-    println! {"\n\n\n"}
-    println!("{}", response);
-    println! {"\n\n\n"}
-    */
-
-    // ugh, let's try to do the rest of it with a local copy for the moment
-    // let path = Path::new("../test_data/trope_page.html");
-    // let display = path.display();
-
-    // let document: Html = match read_html_file(path) {
-    //     Ok(document) => document,
-    //     Err(e) => panic!("Error reading {}: {}", display, e),
-    // };
-
-    // // Create a selector for the element we want
-    // // For the tropes page, every link in a table cell should get us what we want
-    // // This can be done outside of the main loop, since it's the same each time and passed by reference
-    // let trope_selector = Selector::parse("td>a").unwrap();
-
-    // // Select all elements in the document
-    // let trope_links = document.select(&trope_selector);
-
-    // // For every trope, get the inner html (trope_name) and
-    // let mut tropes: Vec<Trope> = Vec::new();
-    // for element in trope_links {
-    //     tropes.push(Trope {
-    //         name: element.inner_html(),
-    //         url: element.value().attr("href").unwrap().to_string(),
-    //     });
-    // }
-
-    // //Test
-    // //println!("{:?}", tropes);
-
-    // // In raw form, there are two non-breaking spaces, possible line breaks and possible
-    // // spaces in the middle. Let's get rid of those for simplicity. Todo: break this into
-    // // seprate function.
-    // for trope in tropes.iter_mut() {
-    //     trope.name = String::from(&trope.name[12..]);
-    //     trope.name.retain(|c| c != '\n' && c != ' ' && c != '\t');
-    // }
-
-    // // Open a file for writing
-    // let output_path = "../test_data/tropes.csv";
-    // let mut output = match File::create(&output_path) {
-    //     Ok(output) => output,
-    //     Err(why) => panic!("Couldn't write to {}: {}", output_path, why),
-    // };
-
-    // // Write all the values to the file
-    // for trope in tropes.iter() {
-    //     let s = format!("{}, {}\n", trope.name, trope.url);
-    //     output
-    //         .write_all(s.as_bytes())
-    //         .expect("Unable to write string");
-    // }
+#[derive(Debug)]
+struct Trope {
+  name: String,
+  url: String,
 }
 
-// fn read_html_file(filename: &Path) -> Result<Html, std::io::Error> {
-//     // Open the file
-//     let mut file = File::open(&filename)?;
+impl fmt::Display for Trope {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "[{},{}]", self.name, self.url)
+  }
+}
 
-//     // Read the file
-//     let mut html = String::new();
-//     file.read_to_string(&mut html)?;
+fn main() {
+  parse_tropelist().unwrap();
+}
 
-//     // Convert to Html
-//     let document = Html::parse_document(&html);
 
-//     Ok(document)
-// }
+/// Download all the pages
+pub fn parse_tropelist() -> Result<(), Box<dyn std::error::Error>> {
+
+  let args = Args::parse_args();
+
+  // Set up input directory in the parent trope-correlate dir
+  let path_dir = path::PathBuf::from("..")
+    .join(trope_lib::DATA_DIR)
+    .join(&args.namespace)
+    .join(&args.pagetype);
+
+
+  // Page parse loop
+  for page in 1..args.max_pages+1 {
+
+    let page_str = page.to_string();
+
+    let file_name = path_dir.clone().join(
+      if args.encrypted {
+        format!("page{}.html.br", &page_str)
+      } else {
+        format!("page{}.html", &page_str)
+      }
+    );
+
+    let document = read_html_file(file_name, args.encrypted).expect("Error reading html file");
+
+    // Create a selector for the element we want
+    // For the tropes page, every link in a table cell should get us what we want
+    // This can be done outside of the main loop, since it's the same each time and passed by reference
+    let trope_selector = Selector::parse("td>a").unwrap();
+
+    // Select all elements in the document
+    let trope_links = document.select(&trope_selector);
+
+    // For every trope, get the inner html (trope_name) and
+    let mut tropes: Vec<Trope> = Vec::new();
+    for element in trope_links {
+      tropes.push(Trope {
+        name: element.inner_html(),
+        url: element.value().attr("href").unwrap().to_string(),
+      });
+    }
+
+    // In raw form, there are two non-breaking spaces, possible line breaks and possible
+    // spaces in the middle. Let's get rid of those for simplicity. Todo: break this into
+    // seprate function.
+    for trope in tropes.iter_mut() {
+      trope.name = String::from(&trope.name[12..]);
+      trope.name.retain(|c| c != '\n' && c != ' ' && c != '\t');
+    }
+
+    // Open a file for writing
+    let output_path = "../test_data/tropes.csv";
+    let mut output = match fs::File::create(&output_path) {
+      Ok(output) => output,
+      Err(why) => panic!("Couldn't write to {}: {}", output_path, why),
+    };
+
+    // Write all the values to the file
+    for trope in tropes.iter() {
+      let s = format!("{}, {}\n", trope.name, trope.url);
+      output
+        .write_all(s.as_bytes())
+        .expect("Unable to write string");
+    }
+
+  }
+
+  Ok(())
+
+}
+
+fn read_html_file(file_name: path::PathBuf, encrypted: bool) -> Result<Html, Box<dyn std::error::Error>> {
+
+  // Input file
+  let html_content = if encrypted {
+
+    let mut enc_content = fs::File::open(file_name)?;
+
+    // Cannot write directly to string; use bytes and then from_utf8
+    let mut html_writer = BufWriter::new(Vec::new());
+
+    // Decode using brotli decompression
+    BrotliDecompress(&mut enc_content, &mut html_writer)?;
+
+    String::from_utf8(html_writer.into_inner()?)?
+
+  } else {
+
+    let mut html_content = String::new();
+    fs::File::open(file_name)?.read_to_string(&mut html_content)?;
+    html_content
+
+  };
+
+  Ok(Html::parse_document(&html_content))
+
+}
