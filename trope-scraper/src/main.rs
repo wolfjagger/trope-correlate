8fd0a9671025c65d8553d79e1046043091d92166
joe <joe@mortalarc.com>
2022-12-15 17:03:27 -0600

    Add error handling (or at least panicking! :P)

--- trope-scraper/src/main.rs
+++ trope-scraper/src/main.rs
@@ -101,22 +101,23 @@ pub fn parse_tropelist() -> Result<(), Box<dyn std::error::Error>> {
 fn read_html_file(file_name: path::PathBuf, encrypted: bool) -> Result<Html, Box<dyn std::error::Error>> {
 
   // Input file
-  let html_content = if encrypted {
+  let html_content: String = if encrypted {
 
-    let mut enc_content = fs::File::open(file_name)?;
+    let mut enc_content = fs::File::open(&file_name).expect(&format!("Cannot open {}", &file_name.display()));
 
     // Cannot write directly to string; use bytes and then from_utf8
     let mut html_writer = BufWriter::new(Vec::new());
 
     // Decode using brotli decompression
-    BrotliDecompress(&mut enc_content, &mut html_writer)?;
+    BrotliDecompress(&mut enc_content, &mut html_writer).expect(&"Error during Brotli decompression");
 
-    String::from_utf8(html_writer.into_inner()?)?
+    let html_bytes = html_writer.into_inner().expect("Into inner error");
+    String::from_utf8(html_bytes).expect("Error writing html string from bytes")
 
   } else {
 
     let mut html_content = String::new();
-    fs::File::open(file_name)?.read_to_string(&mut html_content)?;
+    fs::File::open(file_name).and_then(|mut fi| fi.read_to_string(&mut html_content)).expect("Error writing html string from file");
     html_content
 
   };
