fc2baf28fecefa078b2e1bcf85ab54b10f650dab
joe <joe@mortalarc.com>
2022-12-15 20:30:36 -0600

    Further split lib.rs

---
+++ trope-lib/src/constant.rs
@@ -0,0 +1,1 @@
+pub const DATA_DIR: &str = "test_data";
---
+++ trope-lib/src/util.rs
@@ -0,0 +1,50 @@
+use std::str::FromStr;
+
+pub enum Namespace {
+  Main,
+}
+
+impl ToString for Namespace {
+  fn to_string(&self) -> String {
+    match self {
+      Namespace::Main => "main".to_string()
+    }
+  }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct NamespaceParseError;
+
+impl FromStr for Namespace {
+  type Err = NamespaceParseError;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s.to_lowercase().as_str() {
+      "main" => Ok(Namespace::Main),
+      _ => Err(NamespaceParseError)
+    }
+  }
+}
+
+pub enum Pagetype {
+  Trope,
+}
+impl ToString for Pagetype {
+  fn to_string(&self) -> String {
+    match self {
+      Pagetype::Trope => "trope".to_string()
+    }
+  }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct PagetypeParseError;
+
+impl FromStr for Pagetype {
+  type Err = PagetypeParseError;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s.to_lowercase().as_str() {
+      "trope" => Ok(Pagetype::Trope),
+      _ => Err(PagetypeParseError)
+    }
+  }
+}
