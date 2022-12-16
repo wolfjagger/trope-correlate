10a1ab36c0d1858a04f6ea4cf9fac430425f3c39
joe <joe@mortalarc.com>
2022-12-15 20:36:33 -0600

    Split arg.rs into multiple files

---
+++ trope-lib/src/arg/mod.rs
@@ -0,0 +1,17 @@
+mod trope_download;
+mod trope_scraper;
+
+
+pub use trope_download::{
+  TropeDownloadArgs, TropeDownloadMethod,
+  TropeDownloadPagelist, TropeDownloadTropePage, TropeDownloadTropelist,
+};
+pub use trope_scraper::{
+  TropeScraperArgs, TropeScraperMethod,
+  TropeScraperPagelist,
+};
+
+// trope-download
+
+
+// trope-scraper
---
+++ trope-lib/src/arg/trope_scraper.rs
@@ -0,0 +1,45 @@
+use clap::{Args as ClapArgs, Parser, Subcommand};
+
+use crate::{Namespace, Pagetype};
+
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct TropeScraperArgs {
+  #[command(subcommand)]
+  pub method: TropeScraperMethod
+}
+
+impl TropeScraperArgs {
+  pub fn parse_args() -> Self {
+    Self::parse()
+  }
+}
+
+#[derive(Debug, Subcommand)]
+pub enum TropeScraperMethod {
+  Pagelist(TropeScraperPagelist),
+}
+
+
+/// Downloads index pages in bulk from tvtropes.
+#[derive(Debug, ClapArgs)]
+pub struct TropeScraperPagelist {
+
+  /// If enabled, save an encrypted version of the html
+  #[clap(short, long, value_parser, default_value_t = false)]
+  pub encrypted: bool,
+
+  /// Namespace for page search
+  #[clap(short, long, value_parser, default_value_t = Namespace::Main.to_string())]
+  pub namespace: String,
+
+  /// Pagetype for page search
+  #[clap(short, long, value_parser, default_value_t = Pagetype::Trope.to_string())]
+  pub pagetype: String,
+
+  /// Max number of pages to call for
+  #[clap(short, long, value_parser, default_value_t = 2)]
+  pub max_pages: u8,
+
+}
