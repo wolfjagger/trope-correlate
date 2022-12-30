8e8c241bd2f7bb99e32f58499ef9c39f63874a2a
joe <joe@mortalarc.com>
2022-12-29 23:18:20 -0600

    Add NamedLinkType enum and add regex & once_cell to trope-lib

--- trope-lib/src/lib.rs
+++ trope-lib/src/lib.rs
@@ -5,6 +5,6 @@ mod util;
 
 
 pub use arg::*;
-pub use constant::{DATA_DIR, KNOWN_MEDIA_NAMESPACES};
-pub use serialization::{NamedLink, TropeGeneralJson};
+pub use constant::{DATA_DIR, KNOWN_TROPE_NAMESPACES, KNOWN_MEDIA_NAMESPACES};
+pub use serialization::{NamedLink, NamedLinkType, TropeGeneralJson};
 pub use util::{Namespace, Pagetype};
--- trope-lib/src/serialization.rs
+++ trope-lib/src/serialization.rs
@@ -1,11 +1,66 @@
-use serde::{Deserialize, Serialize};
 use std::fmt;
+
+use once_cell::sync::Lazy;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+
+use crate::{KNOWN_MEDIA_NAMESPACES, KNOWN_TROPE_NAMESPACES};
+
+
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
+pub enum NamedLinkType {
+  Trope,
+  Media,
+  Other,
+}
 
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct NamedLink {
   pub name: String,
   pub url: String,
+  link_type: NamedLinkType,
+}
+
+impl NamedLink {
+
+  pub fn new(name: String, url: String) -> Self {
+    let link_type = Self::calc_link_type(&url);
+    NamedLink{
+      name, url, link_type
+    }
+  }
+
+  pub fn link_type(&self) -> NamedLinkType {
+    self.link_type
+  }
+
+  fn calc_link_type(url: &str) -> NamedLinkType {
+
+    static NAMESPACE_LINK_RE: Lazy<Regex> = Lazy::new(||
+      Regex::new(
+        "/pmwiki/pmwiki.php/([^/]*)/(.*)"
+      ).expect("Error creating namespace regex")
+    );
+
+    match NAMESPACE_LINK_RE.captures(url).and_then(|cap| {
+      let ns = cap.get(1).map(|m| m.as_str());
+      let link_name = cap.get(2).map(|m| m.as_str());
+      ns.and_then(|ns| link_name.map(|ln| (ns, ln)))
+    }) {
+      Some((namespace, _link_name)) => {
+        if KNOWN_TROPE_NAMESPACES.iter().any(|s| s == &namespace) {
+          NamedLinkType::Trope
+        } else if KNOWN_MEDIA_NAMESPACES.iter().any(|s| s == &namespace) {
+          NamedLinkType::Media
+        } else {
+          NamedLinkType::Other
+        }
+      },
+      None => NamedLinkType::Other
+    }
+
+  }
 }
 
 impl fmt::Display for NamedLink {
