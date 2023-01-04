5c82db9f1c5109e6f89c6fff6b8ec3c9edcf43f6
joe <joe@mortalarc.com>
2023-01-04 00:00:08 -0600

    Split trope_download and trope_scrape into lib and main

--- trope-download/src/main.rs
+++ trope-download/src/main.rs
@@ -1,31 +1,8 @@
-mod cmd;
-mod download;
-mod header;
-
-use trope_lib::{TropeDownloadArgs, TropeDownloadMethod};
-use cmd::{
-  download_namespace::save_namespace,
-  download_pagelist::save_pagelist,
-  download_trope::save_trope_page,
-  download_tropelist::save_tropelist,
-};
+use trope_lib::TropeDownloadArgs;
+use trope_download::run;
 
 
-// Download TvTropes pages
 fn main() {
   let args = TropeDownloadArgs::parse_args();
-  match args.method {
-    TropeDownloadMethod::Namespace(method_args) => {
-      save_namespace(method_args).expect("Unhandled download_pagelist error");
-    },
-    TropeDownloadMethod::Pagelist(method_args) => {
-      save_pagelist(method_args).expect("Unhandled download_pagelist error");
-    },
-    TropeDownloadMethod::TropePage(method_args) => {
-      save_trope_page(method_args).expect("Unhandled save_trope_page error");
-    },
-    TropeDownloadMethod::Tropelist(method_args) => {
-      save_tropelist(method_args).expect("Unhandled download_pagelist error");
-    },
-  }
+  run(args);
 }
